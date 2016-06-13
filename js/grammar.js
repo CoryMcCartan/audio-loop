620eaf55fa0451dd61ca2aad6c5a5422d795d335
Cory McCartan <cory.mccartan@gmail.com>
2016-06-12 17:27:59 -0700

    more flexible grammar

--- js/grammar.js
+++ js/grammar.js
@@ -3,16 +3,19 @@
  */
 
 program
-	= info:pacing instructions:instructions* "\n"* { 
+	= title? info:pacing instructions:instructions* "\n"* { 
       return {
         timeSignature: info.timeSignature, 
         tempo: info.tempo, 
         instructions,
       }; 
     }
+
+title
+    = [A-Za-z0-9 \'\"\/\\.,“‘”’]* "\n"+
    
 pacing 
-	= timeSignature:timeSignature sp "at"i sp tempo:tempo {
+	= timeSignature:timeSignature (sp "at"i / ",") sp tempo:tempo {
     	return {
         	timeSignature,
             tempo,
@@ -20,10 +23,22 @@ pacing
     }
 
 timeSignature "a time signature ('4/4')"
-	= num:number "/" denom:number { return [num, denom]; }
+	= ("in"i sp)? num:number "/" denom:number { return [num, denom]; }
+	/ ("in"i sp)? "cut"i sp "time"i { return [2, 2]; }
+	/ ("a"i sp)? "waltz"i { return [3, 4]; }
+	/ ("a"i sp)? "jig"i { return [6, 8]; }
+	/ ("a"i sp)? "reel"i { return [4, 4]; }
 
 tempo "a tempo ('60 bpm')"
 	= tempo:number msp "bpm"i { return tempo; }
+	/ "largo"i { return 40; }
+	/ "adagio"i { return 56; }
+	/ "andante"i { return 76; }
+	/ "moderato"i { return 100; }
+	/ "allegretto"i { return 120; }
+	/ "allegro"i { return 144; }
+	/ "vivace"i { return 176; }
+	/ "presto"i { return 200; }
 
 instructions "an instruction"
     = "\n"+ instr:(record / mute / wait / metronome / end) msp? { return instr; }
